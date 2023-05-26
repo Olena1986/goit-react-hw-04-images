19160b8fa61118b8ab75c291c7f8a43881b9a281
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-05-26 11:29:00 +0200

    img

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,9 +23,11 @@ const initialState = {
 const reducer = (state, action) => {
   switch (action.type) {
     case 'SET_SEARCH_QUERY':
-      return { ...state, searchQuery: action.payload };
+     
+      return { ...state, searchQuery: action.payload, images: [], page: 1 };
     case 'SET_IMAGES':
-      return { ...state, images: [...state.images,...action.payload] };
+      return { ...state, images: action.payload };
+      
     case 'SET_SELECTED_IMAGE':
       return { ...state, selectedImage: action.payload };
     case 'SET_IS_LOADING':
@@ -49,7 +51,8 @@ const reducer = (state, action) => {
       dispatch({ type: 'SET_IS_LOADING', payload: true });
   
       const fetchedImages = await fetchImagesApi(searchQuery, page);
-      dispatch({ type: 'SET_IMAGES', payload:fetchedImages.imgData });
+  
+      dispatch({ type: 'SET_IMAGES', payload: fetchedImages.imgData });
       dispatch({ type: 'SET_TOTAL_PAGE', payload: Math.ceil(fetchedImages.total / 12) });
       dispatch({ type: 'SET_IS_LOADING', payload: false });
   
@@ -85,6 +88,7 @@ useEffect(() => {
     dispatch({ type: 'SET_SEARCH_QUERY', payload: query });
     dispatch({ type: 'SET_IMAGES', payload: [] });
     dispatch({ type: 'SET_PAGE', payload: 1 });
+    fetchImages();
   };
 
   const handleLoadMore = () => {
